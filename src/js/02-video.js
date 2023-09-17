1c15735bf494cbffbbc11478708f6aaa185e9f82
Pantherstich <132139666+Pantherstich@users.noreply.github.com>
2023-09-17 11:23:34 +0300

    second attemp 1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+
+let savedTime = localStorage.getItem('videoplayer-current-time');
+console.log(stopTime);
+
+
+    if (savedTime != null) {
+  player.setCurrentTime(savedTime);
+    }
+
+function timeSaver() {
+player.getCurrentTime().then(function (seconds) {
+localStorage.setItem('videoplayer-current-time', seconds);
+    });
+player.on('timeupdate', throttle(timeSaver, 1000));
+
 
