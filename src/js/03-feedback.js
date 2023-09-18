9363e1e0176ad8d9fd36fa233f444e4bb1b485e6
Pantherstich <132139666+Pantherstich@users.noreply.github.com>
2023-09-18 22:28:16 +0300

    third start

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,7 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+};
