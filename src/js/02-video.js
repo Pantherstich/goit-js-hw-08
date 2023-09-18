46db89eb0228ecabfa497991c8eeb788edf3e340
Pantherstich <132139666+Pantherstich@users.noreply.github.com>
2023-09-19 00:37:42 +0300

    Video %/

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,21 +3,19 @@ import Player from "@vimeo/player";
 
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 
 let savedTime = localStorage.getItem('videoplayer-current-time');
-console.log(stopTime);
-
+console.log(savedTime)
 
     if (savedTime != null) {
   player.setCurrentTime(savedTime);
     }
 
 function timeSaver() {
-player.getCurrentTime().then(function (seconds) {
-localStorage.setItem('videoplayer-current-time', seconds);
-    });
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem('videoplayer-current-time', seconds)
+  });
+}
 player.on('timeupdate', throttle(timeSaver, 1000));
-
-
